a08e28c65be9de86feb6f17630404736804c3a80
Bruno Antico Galin <74074230+BrunoAG77@users.noreply.github.com>
2024-09-03 19:27:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import json
 
 def percentuais(faturamento_mensal):
-    faturamento_total = sum(faturamento_mensal)
+    faturamento_total = sum(faturamento_mensal.values())
     percentual = {}
     for estado, valor in faturamento_mensal.items():
         percent = (valor/faturamento_total) * 100
         percentual[estado] = percent
-        printf(f"{estado} : {percentual:.2f}%")
+        print(f"{estado}: {percent:.2f}%")
     return percentual
 
 def load():
     with open('dados.json','r') as file:
         dados = json.load(file)
-    return dados['faturamento_diario']
+    return dados
 
 def calculo_faturamento(faturamento_daily):
-    faturamento = [dia['faturamento'] for dia in faturamento_diario if dia['faturamento'] > 0]
+    faturamento = [dia['valor'] for dia in faturamento_daily if dia['valor'] > 0]
     menor = min(faturamento)
-    maior = mix(faturamento)
+    maior = max(faturamento)
     media = sum(faturamento)/len(faturamento)
-    dias_acima = sum(1 for dia in faturamento if dia > media)
+    dias_acima = sum(1 for valor in faturamento if valor > media)
     return menor, maior, dias_acima
 
 def fibonacci(n):
@@ -43,18 +43,18 @@ print(soma)
 number = int(input("\nDigite o termo para verificar se pertence à sequência de Fibonacci: \n"))
 fib = fibonacci(number)
 if number in fib:
-    print("O numero",number,"pertence à sequência de Fibonacci")
+    print("\nO numero",number,"pertence à sequência de Fibonacci")
 else:
-    print("O numero",number,"não pertence à sequência de Fibonacci")
+    print("\nO numero",number,"não pertence à sequência de Fibonacci")
 
 #3
 faturamento_daily = load()
-menor, maior, maior = calculo_faturamento(faturamento_diario)
-print("fMenor valor de faturamento:{menor:.2f}\nMaior valor de faturamento:{maior:.2f}\nNúmero de dias acima da média:{dias_acima:.2f}")
+menor, maior, dias_acima = calculo_faturamento(faturamento_daily)
+print(f"\nMenor valor de faturamento: {menor:.2f}\nMaior valor de faturamento: {maior:.2f}\nNúmero de dias acima da média: {dias_acima:.2f}")
 
 #4
 faturamento_mensal = {"SP":67836.43, "RJ":36678.66, "MG":29229.88, "ES":27165.48, "Outros":19849.53}
-percentuais(faturamento_mensal)
+print("\n",percentuais(faturamento_mensal))
 
 #5
 string = str(input("\nDigite uma frase: "))
